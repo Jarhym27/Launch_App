974d20627ffa7da809aad05fd003df56cabef3aa
jocasio2 <joeyohh211@gmail.com>
2023-04-11 13:40:12 -0600

    working on profile

--- front-end/src/component/PayloadProfile.js
+++ front-end/src/component/PayloadProfile.js
@@ -8,57 +8,60 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function PayloadProfile() {
 const [payloadInfo, setPayloadInfo] = useState();
+const [userInfo, setUserInfo] = useState();
+const [payLoadUser, setPayloadUser] = useState()
 
-// useEffect(()=> {
-//     fetch ('http://')
-// })
+useEffect(()=> {
+    fetch ('http://localhost:8080/table/users')
+    .then(res => res.json())
+    .then(data => setUserInfo(data))
+}, [])
+useEffect(()=> {
+    fetch ('http://localhost:8080/table/payloads')
+    .then(res => res.json())
+    .then(data => setPayloadInfo(data))
+}, [])
+console.log(payloadInfo)
+
+let customer = userInfo?.filter((e, i) => e.role == 'payload_user')
+
 
   return (
 <>
 
 <Container fluid className="App py-2 overflow-hidden">
-      <Row className="justify-content-center">Some text here</Row>
-      <Row className="card-example d-flex flex-row flex-nowrap overflow-auto">
-        <Card>
-          <Card.Body>
-            <Card.Title>Payloads</Card.Title>
-            <Card.Text>
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </Card.Text>
-            <footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </footer>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-              This card has supporting text below as a natural lead-in to
-              additional content.{" "}
-            </Card.Text>
-            <footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </footer>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This card has even longer content
-              than the first to show that equal height action.
-            </Card.Text>
-            <footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </footer>
-          </Card.Body>
-        </Card>
+      <Row className="justify-content-center">
+        {customer?.map((user , i) => {
+        return (
+        <Card key={i} className='payloadProfileCard'>
+            <Card.Body>
+                <Card.Title>{user.organization}</Card.Title>
+                <Card.Text>
+                  Username: {user.username}
+                </Card.Text>
+                <footer>
+                    <small className="text-muted">User Created: {user.updated_at}</small>
+                </footer>
+            </Card.Body>
+            </Card>
+        )})}
       </Row>
-      <Row>There's some text in here too</Row>
+      <Row className="card-example d-flex flex-row flex-nowrap overflow-auto">
+        {payloadInfo?.map((payload, i) => {
+return ( 
+        <Card>
+          <Card.Body>
+            <Card.Title>Payload Name (Needs seeding)</Card.Title>
+            <Card.Text>
+                Status: (soon)
+            </Card.Text>
+            <footer>
+              <small className="text-muted">Payload Created: {payload.updated_at}</small>
+            </footer>
+          </Card.Body>
+        </Card>
+        )})}
+      </Row>
     </Container>
 </>
   )
