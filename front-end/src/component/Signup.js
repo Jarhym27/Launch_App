ffe57b74ee29cbcf171f6dd94d4eb0362f565d98
Yeehowdy <kylemersinger67@gmail.com>
2023-04-11 18:17:48 +0200

    Resolving merge conflicts

--- front-end/src/component/Signup.js
+++ front-end/src/component/Signup.js
@@ -76,8 +76,6 @@ return(
        type="password"
        name="password"
        onChange={inputChange}
-       name="password"
-       onChange={inputChange}
        placeholder="Password" />
        <input className="h-full flex flex-wrap items-center justify-center"
        type="password"
@@ -104,7 +102,7 @@ return(
       <button
       type="button"
       onClick={createAccount}
-      onClick={createAccount}
+    
       className="inline-block rounded bg-green-900 px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-green-500 shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-rose-700 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]">Submit</button>
 
     </div>
