718554e50dd80616ee03ac83ec4c8607f87f0b49
IzykPringle <ijpringl@mtu.edu>
2023-04-11 10:14:46 -0600

    fix db tables/seeds

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 8081
+const port = 8080;
 const cors = require('cors')
 const cookieParser = require("cookie-parser")
 const cookieSession = require('express-session')
