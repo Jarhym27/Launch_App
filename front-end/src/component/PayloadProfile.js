1ea90925cb0a421e1d36160bcfcc31c84827680c
jocasio2 <joeyohh211@gmail.com>
2023-04-11 15:13:22 -0600

    added changes to the profile payloads

--- front-end/src/component/PayloadProfile.js
+++ front-end/src/component/PayloadProfile.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './PayloadProfile.css'
 import { useState, useEffect } from 'react'
-import {Container,Row,Col,Card} from 'react-bootstrap'
+import {Container,Row,Col,Card, Button} from 'react-bootstrap'
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
@@ -9,7 +9,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function PayloadProfile() {
 const [payloadInfo, setPayloadInfo] = useState();
 const [userInfo, setUserInfo] = useState();
-const [payLoadUser, setPayloadUser] = useState()
+// const [payLoadUser, setPayloadUser] = useState()
 
 useEffect(()=> {
     fetch ('http://localhost:8080/table/users')
@@ -21,47 +21,73 @@ useEffect(()=> {
     .then(res => res.json())
     .then(data => setPayloadInfo(data))
 }, [])
-console.log(payloadInfo)
+console.log(userInfo)
 
-let customer = userInfo?.filter((e, i) => e.role == 'payload_user')
+let customer = userInfo?.filter((e, i) => e.id == 1)
+let payloads = payloadInfo?.filter((e,i)=> e.payload_user_id == 1)
+
+console.log(payloads)
 
 
   return (
 <>
 
 <Container fluid className="App py-2 overflow-hidden">
-      <Row className="justify-content-center">
+<Row className="justify-content-center profileRow">
+<Col className='profileCol'>
         {customer?.map((user , i) => {
         return (
-        <Card key={i} className='payloadProfileCard'>
-            <Card.Body>
-                <Card.Title>{user.organization}</Card.Title>
+            
+               
+                    <Card key={i} className='payloadProfileCard'>
+                    <Card.Body>
+                        <Card.Title>{user.organization}</Card.Title>
+                        <Card.Text>
+                        Username: {user.username}
+                        </Card.Text>
+                        <footer>
+                            <small className="text-muted">User Created: {user.updated_at}</small>
+                        </footer>
+                    </Card.Body>
+                    </Card>
+                
+           
+        )})}
+        </Col>
+ 
+      
+ 
+    
+    <Col xs={6} >
+    <Row className='payloadsTitle'><h3>Payloads</h3></Row>
+    {payloads?.map((payload, i) => {
+           
+    return ( 
+            <Card >
+            <Card.Body className='payloadsCol'>
+                <Card.Title>{payload.name}</Card.Title>
                 <Card.Text>
-                  Username: {user.username}
+                    Status: (soon)
                 </Card.Text>
                 <footer>
-                    <small className="text-muted">User Created: {user.updated_at}</small>
+                <small className="text-muted">Payload Created: {payload.updated_at}</small>
                 </footer>
             </Card.Body>
             </Card>
+      
+       
         )})}
-      </Row>
-      <Row className="card-example d-flex flex-row flex-nowrap overflow-auto">
-        {payloadInfo?.map((payload, i) => {
-return ( 
-        <Card>
-          <Card.Body>
-            <Card.Title>Payload Name (Needs seeding)</Card.Title>
-            <Card.Text>
-                Status: (soon)
-            </Card.Text>
-            <footer>
-              <small className="text-muted">Payload Created: {payload.updated_at}</small>
-            </footer>
-          </Card.Body>
-        </Card>
-        )})}
-      </Row>
+        </Col>
+        <Col xs lg="2" className='addCol'>
+        <Card className='buttonCard'>
+            <Card.Body>             
+                <Button className='addPayload' variant="outline-primary">+</Button>
+                <Card.Title>Add payload</Card.Title>
+            </Card.Body>
+            </Card>
+        </Col>
+        </Row>
+      
     </Container>
 </>
   )
