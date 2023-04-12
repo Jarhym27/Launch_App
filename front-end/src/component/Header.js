06d297b07d311e673ebc1ecd65470df5df63cc47
IzykPringle <ijpringl@mtu.edu>
2023-04-12 15:52:16 -0600

    mess with header

--- front-end/src/component/Header.js
+++ front-end/src/component/Header.js
@@ -24,26 +24,24 @@ homePath = '/payloadhomepage??'
 return(
   <>
   <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  {/* <div  > */}
     <RocketTakeoffFill className="navbar-brand" color='white' size={50}/>
   <a className="navbar-brand" href="#">L-Uber</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav mr-auto">
+    <ul className="nav navbar-nav">
       <li className="nav-item active">
         <a className="nav-link" href={homePath}>Home</a>
       </li>
       <li className="nav-item">
         <a className="nav-link" href="/AboutUs">About Us</a>
       </li>
+    </ul>
+        <ul className='nav navbar-nav navbar-right'>
       <li class="nav-item">
         <a class="nav-link" href="/" onClick={()=>setUserLogin('')}>Logout</a>
       </li>
-    </ul>
+      </ul>
 
-  </div>
+  {/* </div> */}
 </nav>
 </>
 )
