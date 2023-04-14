5ee80467fa8401c8355095a6f44471d692b94972
IzykPringle <ijpringl@mtu.edu>
2023-04-14 09:58:28 -0600

    filter lsp pads, LVs by lsp id

--- front-end/src/component/lsp_profile/02_lsp_launch_vehicles.js
+++ front-end/src/component/lsp_profile/02_lsp_launch_vehicles.js
@@ -1,21 +1,44 @@
-import React, {  useContext, useEffect } from "react"
+import React, { useState, useContext, useEffect } from "react"
 import { LspDistro } from "./01_lsp_profile_page";
-import { Container, Row, Col, Card, Button } from 'react-bootstrap'
+import { Modal, Container, Row, Col, Card, Button } from 'react-bootstrap'
 import RequestList from "./05_lsp_requests_list";
+import { RocketInfo } from "../../App";
 export default LspLaunchVehicles ;
 
 
 function LspLaunchVehicles() {
-
-    const {launchVehicle, setLaunchVehicle} = useContext(LspDistro)
+  const { userLogin, setUserLogin } = useContext(RocketInfo);
+  const[getInfo ,setGetInfo ] = useState(false)
+   const[launchVehicle, setLaunchVehicle] = useState([]);
+    // const {launchVehicle, setLaunchVehicle} = useContext(LspDistro)
   useEffect(() => {
     fetch('http://localhost:8080/table/launch_vehicles')
       .then(res => res.json())
       .then(data => setLaunchVehicle(data))
   }, [])
 
+  //Add a new vehicle
+const addNewVehicle = (event) =>{
+  fetch("http://localhost:8080/table/launch_vehicle", {
+    method:"POST",
+    body: JSON.stringify({
+      lsp_user_id: userLogin.id,
+      launch_vehicle: event.target.value
+      , cost: event.target.value, 
+      meo_weight: event.target.value, 
+      leo_weight: event.target.value, 
+      geo_weight: event.target.value, 
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    }
+  }).then(() => setGetInfo(true))
+}
 
-const filteredVehicle = launchVehicle?.filter(element => element.booked_status !== "booked")
+
+  //Add a new pad
+
+const filteredVehicle = launchVehicle?.filter(element => element.lsp_user_id === userLogin.id)
 
 return(
   <Row>
@@ -23,7 +46,7 @@ return(
       <Card>
       <Card.Title>
         Launch Vehicle
-        <Button>Add Vehicle</Button>
+        <Button onClick={addNewVehicle}>Add Vehicle</Button>
       </Card.Title>
       {filteredVehicle?.map((vehicle, j) =>{
        return ( <Card.Body key={j}>
@@ -31,8 +54,8 @@ return(
        ID: {vehicle.id}
        <br></br>
        Rocket: {vehicle.launch_vehicle} 
-
-
+       <br></br>
+       Status: {vehicle.booked_status}
         </Card.Text>
         </Card.Body>)
       })}
@@ -41,6 +64,8 @@ return(
   
     <Col >
     <Row>
+
+      
       <button ></button>
         <Col className="col-start-3 col-end-9">
           <Card>
@@ -61,6 +86,10 @@ return(
     </Row>
     </Col>
     </Row>
+
+    // <Modal>
+
+    // </Modal>
 )
 
   
