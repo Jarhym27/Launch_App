d59fc21e41b3b3ae05e78a4fb8b0f8449b834e16
Yeehowdy <kylemersinger67@gmail.com>
2023-04-14 18:38:42 +0200

    Got the filters working

--- front-end/src/component/lsp_profile/05_lsp_requests_list.js
+++ front-end/src/component/lsp_profile/05_lsp_requests_list.js
@@ -9,35 +9,64 @@ const RequestList = () =>{
 
   const {launchRequest, setLaunchRequest,lspUser,setLspUser} = useContext(LspDistro)
 
+  const [launchVehicle, setLaunchVehicle] = useState([])
+
   useEffect(() =>{
-    fetch('http://localhost:8080/table/users')
+    fetch('http://localhost:8080/table/launch_vehicles')
     .then(res => res.json())
-    .then(data => setLspUser(data))
+    .then(data => {
+      data = data.filter(element => element.lsp_user_id === userLogin.id)
+      setLaunchVehicle(data)
+      fetch('http://localhost:8080/join/launch_requests')
+      .then(res => res.json())
+      .then(data => {
+        let rocketIDs = launchVehicle.map(e => e.id)
+        data = data.filter(element => rocketIDs.includes(element.launch_vehicle_id))
+        setLaunchRequest(data)
+        console.log(data)
+      })
+    })
+    .then(() => {
+      fetch('http://localhost:8080/table/users')
+      .then(res => res.json())
+      .then(data => {
+        let activeUsers = launchRequest.map(e => e.payload_user_id)
+        setLspUser(data.filter(element => element.role !== 'lsp_user' && activeUsers.includes(element.id)))
+      })  
+    })
   },[])
-  useEffect(() =>{
-  fetch('http://localhost:8080/join/launch_requests')
-  .then(res => res.json())
-  .then(data => {setLaunchRequest(data); console.log(data)})
-},[])
 
-console.log(launchRequest)
-
-let currentCustomer = lspUser?.filter(element => element.role !== 'lsp_user')
 
 return(
 <Row>
   <Col className="col-start-9 col-end-12">
     {userLogin.username}
-  {currentCustomer?.map((user, i) => {
+  {lspUser?.map((user, i) => {
     return(
     <Card key={i}>
     <Card.Body>
       <Card.Title border="danger">
       Launch Requests from {user.username} 
       </Card.Title>
-      <br />
-      <div>launch request 002</div>
-      <div>launch request 003</div>
+      {launchRequest?.map((e, i) => {
+        if(e.payload_user_id == user.id){
+          return(
+            <div className="border">
+              <p>
+                Launch Date: {e.launch_date}<br/>
+                Launch Site: {e.launch_site}<br/>
+                Launch Vehicle: {e.launch_vehicle}<br/>
+                Payload: {e.name}<br/>
+                Orbit: {e.orbital_requirement}<br/>
+                Weight: {e.weight} Tons<br/>
+                Request Status: {e.request_status}<br/>
+                
+
+              </p>
+            </div>
+          )
+        }
+      })}
     </Card.Body>
     </Card>
   )
