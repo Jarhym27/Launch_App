a7728e6f5e202917867171f286d2e9077fd527b5
Jarhym <jarhym.christoper@spaceforce.mil>
2023-04-20 13:06:47 -0600

    small changes in the rockets and clean up

--- front-end/src/component/Animated_Rocket.js
+++ front-end/src/component/Animated_Rocket.js
@@ -2,18 +2,23 @@ import React from "react"
 import {RocketTakeoffFill} from "react-bootstrap-icons";
 import "../css/Animated_Rocket.css"
 import {GiEvilMoon} from "react-icons/gi"
+import {RocketFill} from "react-bootstrap-icons"
+
+import {GoFlame} from "react-icons/go"
 
 const AnimeRocket = () =>{
-
+  
 return(
   <>
-    <div className="container">
+    <div className="containerRocket">
       <GiEvilMoon className="moon" color='dark-gray' size={100}/> 
        </div>
       <div className="orbit">
-        <RocketTakeoffFill className="image_launch" color='red' size={100}>
-          <div className="window"></div>
-          </RocketTakeoffFill>
+        <RocketFill className="image_launch" color='red' size={100} >
+          <div className="windowRocket"></div>
+          </RocketFill >
+          
+
        </div>         
   </>
     
